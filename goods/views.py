72fc4c02bc594812fb8150624caf3be9f118a984
Avaake <133997731+Avaake@users.noreply.github.com>
2024-09-07 21:18:06 +0300

    create urls in good dirs and update urls in parent dirs. create handlers in views.py

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def catalog(request):
+    return render(
+        request=request,
+        template_name="goods/catalog.html",
+    )
+
+
+def product(request):
+    return render(
+        request=request,
+        template_name="goods/product.html",
+    )
