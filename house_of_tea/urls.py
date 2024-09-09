e5c1d2861546c03fb85489f868fe0ad8e3441c05
Avaake <133997731+Avaake@users.noreply.github.com>
2024-09-09 20:18:47 +0300

    added transition to each product

--- house_of_tea/urls.py
+++ house_of_tea/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("main.urls"), name="main"),
-    path("catalog/", include("goods.urls"), name="catalog"),
+    path("", include("main.urls", namespace="main")),
+    path("catalog/", include("goods.urls", namespace="catalog")),
 ]
 
 
