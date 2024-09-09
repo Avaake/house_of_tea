0ccd0330b5c0a10facaf540f6ec7da8035b984bf
Avaake <133997731+Avaake@users.noreply.github.com>
2024-09-09 18:02:02 +0300

    create Products model

--- goods/models.py
+++ goods/models.py
@@ -5,12 +5,14 @@ class Categories(models.Model):
     name = models.CharField(
         max_length=50,
         unique=True,
-        null=False,
-        blank=False,
     )
 
     slug = models.SlugField(
-        max_length=255, unique=True, null=True, blank=True, verbose_name="URL"
+        max_length=255,
+        unique=True,
+        null=True,
+        blank=True,
+        verbose_name="URL",
     )
 
     class Meta:
@@ -21,3 +23,35 @@ class Categories(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="Назва")
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        null=True,
+        blank=True,
+        verbose_name="URL",
+    )
+
+    description = models.TextField(null=True, blank=True, verbose_name="Опис")
+    image = models.ImageField(
+        upload_to="goods_images", null=True, blank=True, verbose_name="Зображення"
+    )
+    price = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name="Ціна"
+    )
+    discount = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name="Знижка у %"
+    )
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Кількість товару")
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукти"
+
+    def __str__(self):
+        return f"{self.name} - Кількість {self.quantity}"
