c68f21a36219f7622fb11fb4c4a3aa57a14d10a1
Avaake <133997731+Avaake@users.noreply.github.com>
2024-09-12 14:26:19 +0300

    redefine the user model

--- users/models.py
+++ users/models.py
@@ -10,21 +10,6 @@ class User(AbstractUser):
         verbose_name="Аватар",
     )
 
-    groups = models.ManyToManyField(
-        "auth.Group",
-        related_name="custom_user_set",
-        blank=True,
-        help_text="The groups this user belongs to.",
-        verbose_name="groups",
-    )
-    user_permissions = models.ManyToManyField(
-        "auth.Permission",
-        related_name="custom_user_permissions_set",
-        blank=True,
-        help_text="Specific permissions for this user.",
-        verbose_name="user permissions",
-    )
-
     class Meta:
         db_table = "user"
         verbose_name = "Користувач"
