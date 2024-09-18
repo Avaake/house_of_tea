d4566547bebae9a65159d865d0e9b9565ea87c2b
Avaake <133997731+Avaake@users.noreply.github.com>
2024-09-18 18:05:43 +0300

    converted cart_change to jquery

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 
+from carts.templatetags.carts_tags import user_carts
 from carts.utils import get_user_carts
 from goods.models import Products
 from carts.models import Cart
@@ -35,8 +36,27 @@ def cart_add(request):
     return JsonResponse(response_data, safe=False)
 
 
-def cart_change(request, product_slug):
-    pass
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+    cart.quantity = quantity
+    cart.save()
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html",
+        {"carts": cart},
+        request=request,
+    )
+    response_data = {
+        "message": "Кількіть товару було зміненно",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_remove(request):
