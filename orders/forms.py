8f3f9268930ff4accb545b2e859bdf854a67fd8d
Avaake <133997731+Avaake@users.noreply.github.com>
2024-09-27 20:02:42 +0300

    validation phone number

--- orders/forms.py
+++ orders/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 
 
@@ -18,3 +19,16 @@ class CreateOrderForm(forms.Form):
             ("1", "True"),
         ]
     )
+
+    def clean_phone_number(self):
+        data = self.cleaned_data["phone_number"]
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефону повинен містити лише цифри")
+
+        pattern = re.compile(r"^\d{10}$")
+        if not pattern.match(data):
+            raise forms.ValidationError(
+                "Невірни формат номеру. Введіть 10 цифр без коду країни"
+            )
+        return data
