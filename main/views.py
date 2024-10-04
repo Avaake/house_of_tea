9a27ae0e37854041a269b95cacd2c80f73b852d1
Avaake <133997731+Avaake@users.noreply.github.com>
2024-10-04 15:36:50 +0300

    converted main/view to view classes

--- main/views.py
+++ main/views.py
@@ -1,31 +1,50 @@
 from django.shortcuts import render
-from unicodedata import category
-
-from goods.models import Categories
+from django.views.generic import TemplateView
 
 
-def index(request):
+class IndexView(TemplateView):
+    template_name = "main/index.html"
 
-    context = {
-        "title": "Home - Головна",
-        "content": "Магазин чаю House of Tea",
-    }
-
-    return render(
-        request=request,
-        template_name="main/index.html",
-        context=context,
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "HoT - Головна"
+        context["content"] = "Магазин чаю House of Tea"
+        return context
 
 
-def about(request):
-    context = {
-        "title": "Home - О нас",
-        "content": "О Нас",
-        "text_on_page": "Тут ти зможеш придбати найкращі чаї Китаю",
-    }
-    return render(
-        request=request,
-        template_name="main/about.html",
-        context=context,
-    )
+class AboutView(TemplateView):
+    template_name = "main/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "HoT - О нас"
+        context["content"] = "О нас"
+        context["text_on_page"] = "Тут ти зможеш придбати найкращі чаї Китаю"
+        return context
+
+
+# def index(request):
+#
+#     context = {
+#         "title": "Home - Головна",
+#         "content": "Магазин чаю House of Tea",
+#     }
+#
+#     return render(
+#         request=request,
+#         template_name="main/index.html",
+#         context=context,
+#     )
+
+
+# def about(request):
+#     context = {
+#         "title": "Home - О нас",
+#         "content": "О Нас",
+#         "text_on_page": "Тут ти зможеш придбати найкращі чаї Китаю",
+#     }
+#     return render(
+#         request=request,
+#         template_name="main/about.html",
+#         context=context,
+#     )
